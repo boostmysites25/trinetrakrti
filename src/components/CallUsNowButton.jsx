30793cf77fca307342e7f5a663d1fec38ebf52b1
Pranav <mpranavprem@gmail.com>
2025-05-20 11:59:28 +0530

    changes

--- src/components/CallUsNowButton.jsx
+++ src/components/CallUsNowButton.jsx
@@ -6,7 +6,7 @@ import { companyDetails } from "../data/constant";
 const CallUsNowButton = () => {
   return (
     <Link
-      to={`tel:+${companyDetails.phone}`}
+      to={`tel:${companyDetails.phone}`}
       className="w-[4rem] z-30 hover:-translate-y-1 h-[4rem] cursor-pointer rounded-full border-4 border-white bg-primary hover:bg-secondary fixed bottom-7 left-7 flex items-center justify-center transition-all duration-300"
     >
       <PiPhoneCallFill className="text-white text-4xl" />
